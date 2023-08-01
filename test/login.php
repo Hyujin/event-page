896419591555adc7dd4cf98c22e653449a576e24
Yujin <eugenecortes.work@gmail.com>
2023-08-02 07:52:47 +0800

    event-page files

---
+++ test/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Here</title>
+</head>
+<body>
+    <form action="auth.php">
+        <label for="username">Username</label>
+        <input type="text">
+        <br>
+        <label for="password">Password</label>
+        <input type="password">
+        <button type="Submit">Submit</button>
+    </form>
+</body>
+</html>
